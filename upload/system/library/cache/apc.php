44a8a5c6dafc6d8f8bb7ac250241fa3489370559
condor2 <condor20_05@yahoo.it>
2021-04-09 16:00:53 +0300

    APC cache file updated like 4.0.0.0b

--- upload/system/library/cache/apc.php
+++ upload/system/library/cache/apc.php
@@ -13,17 +13,23 @@ class APC {
 		return $this->active ? apc_fetch(CACHE_PREFIX . $key) : false;
 	}
 
-	public function set($key, $value) {
-		return $this->active ? apc_store(CACHE_PREFIX . $key, $value, $this->expire) : false;
+	public function set($key, $value, $expire = '') {
+		if (!$expire) {
+			$expire = $this->expire;
+		}
+
+		return $this->active ? apc_store(CACHE_PREFIX . $key, $value, $expire) : false;
 	}
 
 	public function delete($key) {
 		if (!$this->active) {
 			return false;
 		}
-		
+
 		$cache_info = apc_cache_info('user');
+
 		$cache_list = $cache_info['cache_list'];
+
 		foreach ($cache_list as $entry) {
 			if (strpos($entry['info'], CACHE_PREFIX . $key) === 0) {
 				apcu_delete($entry['info']);
